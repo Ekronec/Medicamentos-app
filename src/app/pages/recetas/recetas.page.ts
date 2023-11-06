a129740a83109ab8285505248e3371caa3109d83
Ekronec <besa.eitan.r@gmail.com>
2023-11-06 15:09:16 -0300

    Annadido el json con busqueda de medicamentos y implementacion de lista de medicamentos dentro de el

--- src/app/pages/recetas/recetas.page.ts
+++ src/app/pages/recetas/recetas.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JsonDataService } from '../../services/json-data.service';
+import medicamentosData from '../../../assets/medicamentos.json'
 
 @Component({
   selector: 'app-recetas',
@@ -11,6 +12,10 @@ export class RecetasPage implements OnInit {
 
   jsonData: any;
   loading: boolean = false;
+  private medicamentos: any[] = medicamentosData.records;
+  busqueda: string = '';
+  sugerencias: string[] = [];
+  medicamentosSeleccionados: string[] = [];
 
   constructor(private jsonDataService: JsonDataService) { }
 
@@ -26,6 +31,25 @@ export class RecetasPage implements OnInit {
       this.loading = false; // Desactiva la animación de carga cuando se completa la solicitud
     });
   }
+
+  buscarMedicamentos(termino: string): void {
+    if (termino.trim() === '') {
+      this.sugerencias = []; // Si el término de búsqueda está vacío, borra las sugerencias.
+      return;
+    }
+  
+    this.sugerencias = this.medicamentos
+      .filter((medicamento: any) =>
+        medicamento[2].toLowerCase().includes(termino.toLowerCase())
+      )
+      .map((medicamento: any) => medicamento[2]);
+  }
+  
+  agregarMedicamentoSeleccionado(medicamento: string): void {
+    this.medicamentosSeleccionados.push(medicamento);
+    this.busqueda = ''; // Borra el campo de búsqueda después de agregar el medicamento.
+  }
+  
 
 
 }
