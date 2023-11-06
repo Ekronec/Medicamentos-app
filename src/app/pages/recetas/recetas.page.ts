ae6de84f016d65b23e40848f6e7b8513d36f0382
Ekronec <besa.eitan.r@gmail.com>
2023-11-06 12:57:05 -0300

    Annadida API de medicamentos

--- src/app/pages/recetas/recetas.page.ts
+++ src/app/pages/recetas/recetas.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonDataService } from '../../services/json-data.service';
 
 @Component({
   selector: 'app-recetas',
   templateUrl: './recetas.page.html',
   styleUrls: ['./recetas.page.scss'],
 })
+
 export class RecetasPage implements OnInit {
 
-  constructor() { }
+  jsonData: any;
+  loading: boolean = false;
+
+  constructor(private jsonDataService: JsonDataService) { }
 
   ngOnInit() {
+    this.loadData();
   }
+
+  loadData() {
+    this.loading = true;
+
+    this.jsonDataService.getJsonData().subscribe(data => {
+      this.jsonData = data;
+      this.loading = false; // Desactiva la animación de carga cuando se completa la solicitud
+    });
+  }
+
 
 }
